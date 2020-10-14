03a12863fa385e0f4392c535a722b497634255ad
Yujing Yan <52253475+LikkaH@users.noreply.github.com>
2020-10-15 06:04:51 +0800

    Create SystemTime.java

---
+++ ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/util/SystemTime.java
@@ -0,0 +1,34 @@
+package ca.mcgill.ecse.flexibook.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SystemTime {
+
+	private static String SysTime;
+
+	//this class is only used for define the system time and date
+	//in clause: Given the system's time and date is ""
+	
+	//please be free to edit this file if you have a better idea
+	//features related: ViewAppointmentCalendar; SetUpBusinessInfo; MakeAppointment
+	public SystemTime(String systime, boolean istest) {
+		if(istest) {
+			this.SysTime = systime;
+			
+		}else {
+			SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-ddZ");
+			Date date = new Date();
+			this.SysTime = dateFormatter.format(date);
+		}
+	}
+	
+	public String getSysTime() {
+		return SysTime;
+	}
+	
+	public void setSysTime(String systime) {
+		this.SysTime = systime;
+	}
+
+}

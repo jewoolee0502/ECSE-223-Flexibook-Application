d793c9bddf8fa2aad85d77db8234c76f4f20b940
jewoolee0502 <jwcairo0502@gmail.com>
2020-10-18 05:36:04 +0200

    Fixed Javadoc for FlexibookController class

--- ca.mcgill.ecse.flexibook/src/test/java/ca/mcgill/ecse/flexibook/features/UpdateCustomerOrOwnerAccountStepDefinition.java
+++ ca.mcgill.ecse.flexibook/src/test/java/ca/mcgill/ecse/flexibook/features/UpdateCustomerOrOwnerAccountStepDefinition.java
@@ -9,22 +9,28 @@ public class UpdateCustomerOrOwnerAccountStepDefinition {
 
 	@Given("an owner account exists in the system with username {string} and password {string}")
 	public void an_owner_account_exists_in_the_system_with_username_and_password(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
+		
+		// Write code here that turns the phrase above into concrete actions
+	    //throw new io.cucumber.java.PendingException();
 	}
 	
 
 	
 	@When("the user tries to update account with a new username {string} and password {string}")
 	public void the_user_tries_to_update_account_with_a_new_username_and_password(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
+		
+		// Write code here that turns the phrase above into concrete actions
+	    //throw new io.cucumber.java.PendingException();
 	}
 	
 	@Then("the account shall not be updated")
 	public void the_account_shall_not_be_updated() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
+		
+		// Write code here that turns the phrase above into concrete actions
+	    //throw new io.cucumber.java.PendingException();
 	}
 
 }

4e369ee0c1152c326bcea1fc3615693eac1d3dfe
Haipeng Yue <benyue2000@gmail.com>
2020-10-22 14:00:24 -0400

    model regenerated

--- ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/model/Service.java
+++ ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/model/Service.java
@@ -33,9 +33,7 @@ public class Service extends BookableService
   // INTERFACE
   //------------------------
 
-
-
-public boolean setDuration(int aDuration)
+  public boolean setDuration(int aDuration)
   {
     boolean wasSet = false;
     duration = aDuration;
